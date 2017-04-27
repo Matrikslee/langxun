03662ca1bb12c351eb832aa1fbfbd310bced5c0f
matrikslee <im.lechain@gmail.com>
2017-04-27 17:12:38 +0800

    first push

--- main.cpp
+++ main.cpp
@@ -5,38 +5,17 @@
 #include <time.h>
 #include "langxun.h"
 #define INF 1e9
-#define Depth 3///搜索深度
+#define Depth 4///搜索深度
 using namespace std;
 int Color;///1代表黑,2代表白
-int max_value=-INF;///初始化最初的局面估值
+#define _DEBUG_
+#undef _DEBUG_
 int Map[21][21],Copy_Map[Depth+1][21][21];///局面
 int Step_x[500],Step_y[500];///历史着法
 ///八个方向,正上，正下，正左，正右，左上，左下，右上，右下
 #define MAX_DIRNUM (8)
 int dirx[MAX_DIRNUM]={-1,1,0,0,-1,1,-1,1};
 int diry[MAX_DIRNUM]={0,0,-1,1,-1,-1,1,1};
-///估值表
-int Map_value[19][19]={
-  {9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9},
-  {9,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
-  {9,1,9,1,5,1,8,1,7,1,5,1,1,1,1,2,1,1,9},
-  {9,1,1,5,5,5,5,5,5,5,5,5,5,5,5,5,1,1,9},
-  {9,1,8,5,3,2,1,1,1,1,1,1,1,2,3,5,1,1,9},
-  {9,1,1,5,1,1,1,1,1,1,1,1,1,1,3,5,1,1,9},
-  {9,1,8,5,1,1,1,9,9,9,9,9,1,1,3,5,1,1,9},
-  {9,1,1,5,1,1,1,9,1,1,1,9,1,1,3,5,1,1,9},
-  {9,1,7,5,1,1,1,9,1,1,1,9,1,1,3,5,1,1,9},
-  {9,1,1,5,1,1,1,9,1,1,1,9,1,1,2,5,2,1,9},
-  {9,1,5,5,1,1,1,9,1,1,1,9,1,1,2,5,2,1,9},
-  {9,1,1,5,1,1,1,9,9,9,9,9,1,1,2,5,2,1,9},
-  {9,1,8,5,1,1,1,1,1,1,1,1,1,1,2,5,2,1,9},
-  {9,1,5,5,5,1,1,1,1,1,1,1,1,1,5,5,1,1,9},
-  {9,1,5,5,5,1,1,1,1,1,1,1,1,1,5,5,1,1,9},
-  {9,1,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,1,9},
-  {9,1,9,1,8,1,8,1,7,1,7,1,8,1,8,1,1,1,9},
-  {9,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,9},
-  {9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9},
-};
 
 struct node{
     int x,y;
@@ -51,23 +30,20 @@ bool check(int x,int y){
     return !((x>=9&&x<=11)&&(y>=9&&y<=11));
 }
 
-bool Is_Right(int handle,int x,int y,int depth,int color){
-  ///句柄1代表计算出哪些方向有可翻转棋子,句柄2代表判断该点是否是可行解，1代表黑棋，2代表白棋
-  int opp_color = color^3;
+bool Is_Right(int x,int y,int depth,int color){
   int dirnum = 0;
-  if(handle==1)
-    Move[depth][MoveDepth[depth]].DirNum=0;
+  Move[depth][MoveDepth[depth]].DirNum=0;
 
   for(int d=0; d<MAX_DIRNUM; ++d) {
     bool flag = false;
     for (int i=x+dirx[d],j=y+diry[d]; check(i,j); i+=dirx[d], j+=diry[d]){
-      if(!Map[i][j]) { break; }
-      if(Map[i][j]==opp_color) {
+      if(!Map[i][j]) { break; }  // 有空子,不能下
+      if(Map[i][j]==(color^3)) { // 碰到对方的子,可行
         flag =true;
         continue;
       }
+      //碰到己方的子，结束
       if(flag) {
-        if(handle!=1) { return true; }
         Move[depth][MoveDepth[depth]].MoveDir[dirnum++] = d;
       }
       break;
@@ -78,35 +54,30 @@ bool Is_Right(int handle,int x,int y,int depth,int color){
 }
 
 ///句柄1代表寻找可行着法并入栈，句柄2代表估值计算sum(估值表*每个落子点)
-int CanMove(int handle,int depth,int color){
-  int evaluate=0,cnt=0;
-  if(handle==1)
-    MoveDepth[depth]=0;
+int CanMove(int depth,int color){
+  MoveDepth[depth]=0;
   for(int i=1;i<=19;i++) {
     for(int j=1;j<=19;j++) {
-      if(Map[i][j]==0&&check(i,j)&&Is_Right(handle,i,j,depth,color)){
+      if(Map[i][j]==0&&check(i,j)&&Is_Right(i,j,depth,color)){
         ///找到一个起点
-        if(handle==1){
-          Move[depth][MoveDepth[depth]].x=i;///允许的着法，入栈
-          Move[depth][MoveDepth[depth]].y=j;
-          MoveDepth[depth]++;
-        } else {
-          evaluate+=Map_value[i-1][j-1];
-        }
-        cnt++;
+        Move[depth][MoveDepth[depth]].x=i;///允许的着法，入栈
+        Move[depth][MoveDepth[depth]].y=j;
+        MoveDepth[depth]++;
       }
     }
   }
-  return handle==1?cnt:evaluate;///句柄1返回着法个数，句柄2返回估值
+  return MoveDepth[depth];
 }
 
-void MakeMove(int depth,int color){
+int MakeMove(int depth,int color){
   ///模拟落子
   ///赋值棋局副本
   for(int i=1;i<=19;i++)
     for(int j=1;j<=19;j++)
       Copy_Map[depth][i][j]=Map[i][j];
 
+  //  int casenum = MoveDepth[depth]-1;
+  //随机选择一个落子方案,以获得较大的速度和期望
   int casenum = rand()%MoveDepth[depth];
 
   int x = Move[depth][casenum].x;
@@ -122,6 +93,7 @@ void MakeMove(int depth,int color){
       y += diry[d];
     }
   }
+  return casenum;
 }
 
 void UnMakeMove(int depth){///取消落子
@@ -131,47 +103,41 @@ void UnMakeMove(int depth){///取消落子
 }
 
 int Evaluation(int color){
-  int obj_val = 0, opp_val = 0;
+  const char sign[2] = {-1, 1};
+  int value = 0;
 
   for ( int i = 1; i <= 19; ++ i ) {
     for ( int j = 1; j <= 19; ++j ) {
-      if( Map[i][j] == color ) {
-        obj_val += Map_value[i-1][j-1];
-      }
-      if( Map[i][j] == (color^3)) {
-        opp_val += Map_value[i-1][j-1];
-      }
+      if(!Map[i][j]) continue;
+      value += sign[Map[i][j]==color];//*Map_value[i-1][j-1];
     }
   }
 
-  return obj_val-opp_val;
+  return value;
 }
 
 int Alpha_Beta(int depth, int alpha, int beta, int color){
   ///color代表即将要行动的那方
-  if(depth==0)///抵达搜索深度
-    return Evaluation(color);
+  if(depth==0||!CanMove(depth,color))///抵达搜索深度
+    return Evaluation(color^3);
 
-  ///CanMove着法生成器，顺带将所有着法的翻转方向记录下
-  if(!CanMove(1,depth,color)){///某个玩家无法下棋
-    return Evaluation(color^3); // 应该返回对手的估值
-  }
   int maxval = -INF;
   ///否则，CanMove函数选择出该color所有可以下的点，通过栈返回
   while(MoveDepth[depth]) {
-    MakeMove(depth,color);///模拟落子
+    int casenum = MakeMove(depth,color);///模拟落子
     int val=-Alpha_Beta(depth-1,-beta,-alpha,color^3);
     UnMakeMove(depth);
-    if(val>alpha) {
-      if(val>=beta)
-            return beta;
-      alpha=val;
-    }
-    if(val>maxval)
+    Move[depth][casenum] = Move[depth][--MoveDepth[depth]];
+    if(val>maxval) {
       maxval = val;
-    MoveDepth[depth]--;
+    }
+    if(maxval > alpha) {
+      alpha=maxval;
+    }
+    if(maxval >= beta) {
+      break;
+    }
   }
-
   return maxval;
 }
 
@@ -183,17 +149,19 @@ void init(){///初始化
 }
 
 int main(){
+#ifdef _DEBUG_
   clock_t t1, t2;
   t1 = clock();
+#endif
   init();///初始化
-  if(!CanMove(1,Depth,Color)) {
+  if(!CanMove(Depth,Color)) {
     outputAnswer(0,0);
     return 0;
   }
-
+  int max_value=-INF;///初始化最初的局面估值
   ///否则，CanMove函数选择出该color所有可以下的点，通过栈返回
   while(MoveDepth[Depth]) {
-    MakeMove(Depth,Color);///模拟落子
+    int casenum = MakeMove(Depth,Color);///模拟落子
     int tmp_value=Alpha_Beta(Depth-1,-INF,INF,Color^3);
     if(tmp_value>max_value) {
       Ans.x=Move[Depth][MoveDepth[Depth]-1].x;
@@ -201,12 +169,14 @@ int main(){
       max_value=tmp_value;
     }
     UnMakeMove(Depth);///取消落子
-    MoveDepth[Depth]--;
+    Move[Depth][casenum] = Move[Depth][--MoveDepth[Depth]];
   }
   outputAnswer(Ans.x,Ans.y);
+#ifdef _DEBUG_
   t2 = clock();
   FILE* fp = fopen("debug", "a");
   fprintf(fp,"spent %.3lf ms by main\n",((float)(t2 - t1) / 1000000.0F ) * 1000);
   fclose(fp);
+#endif
   return 0;
 }
